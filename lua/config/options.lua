d8ed0cff8806b1c38fa273521a379e96ac55288d
Dylan Reid <dgreid@rivosinc.com>
2024-08-02 01:14:14 -0700

    remove more default cruft

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+-- undo non old vim defaults that annoy me
+vim.opt.cursorline = false
+vim.opt.ignorecase = false
+vim.opt.linebreak = false
+vim.opt.list = false
+vim.opt.number = false
 vim.opt.relativenumber = false
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 8
+vim.opt.wrap = true
+
+vim.o.signcolumn = "number"
