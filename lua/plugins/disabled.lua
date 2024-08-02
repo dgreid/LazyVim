fba39e22f7b113a3eceb7b0448e7f3bb72e25403
Dylan Reid <dgreid@rivosinc.com>
2024-08-02 01:14:14 -0700

    remove more default cruft

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  -- disable janky buffer and tab page tabs.
+  { "akinsho/bufferline.nvim", enabled = false },
+  --- Don't need funky vertical bars for indentaiton
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+}
