67df510adbe9a9cf53f23bcc11e0dff360edd922
Dylan Reid <dgreid@rivosinc.com>
2024-08-01 23:46:35 -0700

    remove annoying default mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,14 @@
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
 map("n", ",e", ":e <C-R>=expand(\"%:p:h\") . \"/\" <CR>", { desc = "Open in file directory" })
+
+-- Remove annoying Alt line switching Default
+vim.keymap.del({"n", "i", "v"}, "<A-j>")
+vim.keymap.del({"n", "i", "v"}, "<A-k>")
+
+-- Ctrl-S if for notepad muscle memory I don't have.
+vim.keymap.del({ "i", "x", "n", "s" }, "<C-s>")
+-- Don't need a "new file" shortcut
+vim.keymap.del("n", "<leader>fn")
+-- I know how to exit vim...
+vim.keymap.del("n", "<leader>qq")
