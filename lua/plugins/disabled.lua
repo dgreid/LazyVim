32489099d255696032c4ede934f0bddf8d8e8760
Dylan Reid <dgreid@rivosinc.com>
2024-08-03 00:21:26 -0700

    disable more

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,8 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   --- Don't need funky vertical bars for indentaiton
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  --- The autocomplete is super annoying with enter to complete.
+  { "hrsh7th/nvim-cmp", enabled = false },
+  --- hate matching pairs
+  { "echasnovski/mini.pairs", enabled = false },
 }
