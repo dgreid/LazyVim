62bbf70f1b508333a90a21e908e765e97c972ad9
Dylan Reid <dgreid@rivosinc.com>
2024-08-05 16:56:43 -0700

    disable folke/flash

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -7,4 +7,6 @@ return {
   { "hrsh7th/nvim-cmp", enabled = false },
   --- hate matching pairs
   { "echasnovski/mini.pairs", enabled = false },
+  --- regular seach is fine and the default keymaps are intrusive (s = switch)
+  { "folke/flash.nvim", enabled = false },
 }
